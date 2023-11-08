fc89a50bef5a68b3ea3343e3c3f207e8081dc098
sim1-99 <simonechiarella99@gmail.com>
2023-11-08 12:38:06 +0100

    Create utils.py

---
+++ modules/utils.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Utils.
+
+This module contains a set of funtions used for file management (e.g.,
+for reading and saving files).
+"""
+
+__author__ = 'Simone Chiarella'
+__email__ = 'simone.chiarella@studio.unibo.it'
+
+from pathlib import Path
+import zipfile
+import shutil
+import SimpleITK as sitk
