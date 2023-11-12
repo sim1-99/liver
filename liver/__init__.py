820fb77cddc7dcc77ace2b0aa38c6dfffbc87287
sim1-99 <simonechiarella99@gmail.com>
2023-11-12 16:36:00 +0100

    Create __init__.py and __main__.py

---
+++ liver/__init__.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""__init__.py for CL application."""
+
+__author__ = 'Simone Chiarella'
+__email__ = 'simone.chiarella@studio.unibo.it'
---
+++ liver/__main__.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+__main__.py for CL application.
+"""
+
+__author__ = 'Simone Chiarella'
+__email__ = 'simone.chiarella@studio.unibo.it'
