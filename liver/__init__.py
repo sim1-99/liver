aff928277b7e9c26209385fd5a375ab63a8bf0d7
sim1-99 <simonechiarella99@gmail.com>
2023-11-12 18:38:22 +0100

    Change main directory name

--- liver/__init__.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""__init__.py for CL application."""
-
-__author__ = 'Simone Chiarella'
-__email__ = 'simone.chiarella@studio.unibo.it'
--- liver/__main__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-__main__.py for CL application.
-"""
-
-__author__ = 'Simone Chiarella'
-__email__ = 'simone.chiarella@studio.unibo.it'
