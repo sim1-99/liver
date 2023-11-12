f1ca2c964daa8ffff3c5bf8269e8b638023ba276
sim1-99 <simonechiarella99@gmail.com>
2023-11-12 18:36:39 +0100

    Change main directory name

---
+++ LiverExtraction/first_region_growing.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+First region growing.
+
+This script imports the CT volume, performs some preprocessing and a first
+region growing on it to extract the liver. Then saves this first segmentation
+in a .nii file.
+"""
+
+__author__ = 'Simone Chiarella'
+__email__ = 'simone.chiarella@studio.unibo.it'
+
+
+import argparse
+import numpy as np
+import SimpleITK as sitk
+
+from LiverExtraction.modules.utils import write_volume
